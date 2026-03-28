ef6d661fe15c31d5fc5f01a50de3e73860c74fca
bernieleow19 <bernieleow@u.nus.edu>
2026-03-28 21:43:54 +0800

    Add transform logic for fact_trips table

--- source/dataops/transform.py
+++ source/dataops/transform.py
@@ -1,8 +1,16 @@
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, coalesce, lit
+from pyspark.sql.functions import col, coalesce, lit, when
 from utils.config import PROCESSED_PATH, get_raw_file_path
 from utils.monitoring import monitor
+
+STANDARD_TRIP_FACT_COLUMNS = [
+    "pickup_datetime", "dropoff_datetime", "pulocationid", "dolocationid", "trip_distance", "fare_amount", "total_amount",
+    "taxi_type", "vendor_id", "hvfhs_license_num", "dispatching_base_num", "originating_base_num", "passenger_count", "ratecode_id",
+    "request_datetime", "on_scene_datetime", "store_and_fwd_flag", "payment_type", "trip_time", "shared_request_flag", "extra",
+    "mta_tax", "improvement_surcharge", "wav_request_flag", "wav_match_flag", "tip_amount", "tolls_amount", "driver_pay",
+    "congestion_surcharge", "airport_fee", "cbd_congestion_fee", "bcf", "sales_tax", "access_a_ride_flag", "shared_match_flag"
+]
 
 @monitor
 def transform_fact(**context):
@@ -61,56 +69,89 @@ def transform_fact(**context):
     if not dfs:
         print("No data found for this month → skipping transform")
         return
+    
+    # standardise each dataset before union
+    standardised_dfs = []
+    for df in dfs:
+        taxi_type = df.schema["taxi_type"]
 
-    # combine datasets
-    combined = dfs[0]
-    for df in dfs[1:]:
-        combined = combined.unionByName(df, allowMissingColumns=True)
+        rename_map = {
+            "VendorID": "vendor_id",
+            "RatecodeID": "ratecode_id",
+            "PULocationID": "pulocationid",
+            "DOLocationID": "dolocationid",
+            "trip_miles": "trip_distance",
+            "base_passenger_fare": "fare_amount",
+            "tips": "tip_amount",
+            "tolls": "tolls_amount"
+        }
 
-    # unify pickup datetime
-    pickup_cols = []
+        for old, new in rename_map.items():
+            if old in df.columns:
+                df = df.withColumnRenamed(old, new)
 
-    if "pickup_datetime" in combined.columns:
-        pickup_cols.append(col("pickup_datetime"))
+        # unify datetime columns
+        df = df.withColumn(
+                "pickup_datetime",
+                coalesce(col("pickup_datetime"), col("tpep_pickup_datetime"))
+            )
 
-    if "tpep_pickup_datetime" in combined.columns:
-        pickup_cols.append(col("tpep_pickup_datetime"))
+        df = df.withColumn(
+                "dropoff_datetime",
+                coalesce(col("dropoff_datetime"), col("tpep_dropoff_datetime"))
+            )
+        
+        # calculating fhvhv total_amount
+        if taxi_type == "fhvhv":
+            df = df.withColumn(
+                "total_amount",
+                coalesce(col("fare_amount"), lit(0)) +
+                coalesce(col("tolls_amount"), lit(0)) +
+                coalesce(col("congestion_surcharge"), lit(0)) +
+                coalesce(col("airport_fee"), lit(0)) +
+                coalesce(col("cbd_congestion_fee"), lit(0))
+            )
 
-    if "lpep_pickup_datetime" in combined.columns:
-        pickup_cols.append(col("lpep_pickup_datetime"))
+        for col_name in STANDARD_TRIP_FACT_COLUMNS:
+            if col_name not in df.columns:
+                df = df.withColumn(col_name, lit(None))
 
-    if pickup_cols:
+        df = df.select(STANDARD_TRIP_FACT_COLUMNS)
+        standardised_dfs.append(df)
+
+    # union datasets
+    combined = standardised_dfs[0]
+    for df in standardised_dfs[1:]:
+        combined = combined.unionByName(df)
+
+    ## data cleaning
+    # critical fields
+    combined = combined.filter(
+        col("pickup_datetime").isNotNull() &
+        col("dropoff_datetime").isNotNull() &
+        col("pulocationid").isNotNull() &
+        col("dolocationid").isNotNull()
+    )
+
+    # invalid trips
+    combined = combined.filter(
+        col("pickup_datetime") < col("dropoff_datetime")
+    )
+
+    # convert flags to boolean
+    FLAG_COLUMNS = [
+        "shared_request_flag", "shared_match_flag", "wav_request_flag", "wav_match_flag", "access_a_ride_flag", "store_and_fwd_flag"
+    ]
+
+    for c in FLAG_COLUMNS:
         combined = combined.withColumn(
-            "pickup_datetime", coalesce(*pickup_cols)
+            c,
+            when(col(c) == "Y", True)
+            .when(col(c) == "N", False)
+            .otherwise(None)
         )
 
-    # unify dropoff datetime 
-    dropoff_cols = []
-
-    if "dropoff_datetime" in combined.columns:
-        dropoff_cols.append(col("dropoff_datetime"))
-
-    if "tpep_dropoff_datetime" in combined.columns:
-        dropoff_cols.append(col("tpep_dropoff_datetime"))
-
-    if "lpep_dropoff_datetime" in combined.columns:
-        dropoff_cols.append(col("lpep_dropoff_datetime"))
-
-    if dropoff_cols:
-        combined = combined.withColumn(
-            "dropoff_datetime", coalesce(*dropoff_cols)
-        )
-
-    # drop old redundant columns
-    for c in [
-        "tpep_pickup_datetime",
-        "lpep_pickup_datetime",
-        "tpep_dropoff_datetime",
-        "lpep_dropoff_datetime",
-    ]:
-        if c in combined.columns:
-            combined = combined.drop(c)
-
+    # Write to trip fact table
     combined = combined.withColumn("year", lit(year))
     combined = combined.withColumn("month", lit(month))
     combined = combined.repartition(8)
