76f53e499b2c1a0a2e1982e1f9e66cf8e3c784a7
myathetchai <myathetchai441@gmail.com>
2026-03-28 20:36:50 +0800

    fix: load.py docker and db connection issue

--- source/dataops/load.py
+++ source/dataops/load.py
@@ -3,6 +3,9 @@ from pyspark.sql import SparkSession
 from utils.config import PROCESSED_PATH
 from utils.db import engine
 from utils.monitoring import monitor
+import logging
+
+logger = logging.getLogger(__name__)
 
 @monitor
 def load_data(**context):
@@ -10,12 +13,18 @@ def load_data(**context):
     year = execution_date.year
     month = execution_date.month
     
+    # spark = SparkSession.builder \
+    #     .appName("Taxi ETL Load") \
+    #     .config(
+    #         "spark.jars.packages",
+    #         "org.postgresql:postgresql:42.7.3"
+    #     ) \
+    #     .getOrCreate()
     spark = SparkSession.builder \
         .appName("Taxi ETL Load") \
-        .config(
-            "spark.jars.packages",
-            "org.postgresql:postgresql:42.7.3"
-        ) \
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3") \
+        .config("spark.driver.extraJavaOptions", "-Djava.net.preferIPv4Stack=true") \
+        .config("spark.executor.extraJavaOptions", "-Djava.net.preferIPv4Stack=true") \
         .getOrCreate()
 
     print(f"Loading data for {year}-{month:02d}")
@@ -32,6 +41,10 @@ def load_data(**context):
 
     jdbc_url = f"jdbc:postgresql://{engine.url.host}:{engine.url.port}/{engine.url.database}"
     
+    # logging
+    logger.info(f"Connecting to JDBC URL: {jdbc_url}")
+    logger.info(f"Host: {engine.url.host}, Port: {engine.url.port}, DB: {engine.url.database}, User: {engine.url.username}")
+
     df.write \
         .format("jdbc") \
         .option("url", jdbc_url) \
