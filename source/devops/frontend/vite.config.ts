ac1d1ae8c0834ccd12c0d24beceac00543b1ee27
purp <pojaroysg29@gmail.com>
2026-04-16 06:10:12 +0800

    allow hosts for frontend

--- source/devops/frontend/vite.config.ts
+++ source/devops/frontend/vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: [
+      'nyc-taxi.app',
+      'www.nyc-taxi.app',
+      'api.nyc-taxi.app',
+      'airflow.nyc-taxi.app',
+      'mlflow.nyc-taxi.app'
+    ],
     watch: {
       ignored: ['**/tsconfig*.json', '**/vite.config.ts']
     }
